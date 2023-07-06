1412882dac5abf1da933af475b0c0820e700169e
[웹개발3팀] 김가영 <doit772@naver.com>
2023-07-06 18:00:16 +0900

    모르는거 메모

--- hackers/publish_2307/memoApp.js
+++ hackers/publish_2307/memoApp.js
@@ -17,14 +17,15 @@ class Item {
     
     DeleteButton.innerText = "삭제";
     DeleteButton.addEventListener("click", () => {
-      memoList.removeChild(div);
+      memoList.removeChild(div); // memoList.remove(div)로 하면 한 번 삭제 버튼 누른 뒤에 메모추가가 안됨
     });
     DeleteButton.classList.add("delete-button");
 
     div.append(Input, DeleteButton);
-    memoList.appendChild(div);
+    memoList.appendChild(div); // memoList.append(div); 도 가능
   }
 }
+
 const checkInput = () => {
   const input = document.getElementById("memo-text");
   if (input.value !== "") {
@@ -32,6 +33,7 @@ const checkInput = () => {
   }
   input.value = "";
 };
+
 const deletememos = () => {
   while (memoList.hasChildNodes()) {
     memoList.removeChild(memoList.firstChild);
