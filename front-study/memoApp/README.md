347ac2e86f118ed37c5d3751d01fe99b47ca9777
[웹개발3팀] 김가영 <doit772@naver.com>
2023-07-13 18:00:57 +0900

    메모앱 작업중

---
+++ front-study/memoApp/README.md
@@ -0,0 +1,5 @@
+# 바닐라 스크립트로 메모앱 만들기
+
+> 참고목록 
+>- [[Javascript] localStorage 사용법 (읽기, 쓰기, 삭제, 키목록 등)](https://hianna.tistory.com/697)
+>- [[JS] localStorage란?(feat. 잘 쓰는 팁)](https://mong-blog.tistory.com/entry/JS-localStorage%EB%9E%80feat-%EC%9E%98-%EC%93%B0%EB%8A%94-%ED%8C%81)
--- hackers/publish_2307/memoApp.js
+++ hackers/publish_2307/memoApp.js
@@ -10,25 +10,41 @@ class Item {
   newmemo(memo) {
     const div = document.createElement("div");
     const Input = document.createElement("input");
-    Input.value = memo;
+    Input.value = memo || localStorage.getItem("memoData") || "";
     Input.disabled = true;
 
     const DeleteButton = document.createElement("button");
     
-    DeleteButton.innerText = "-";
+    DeleteButton.innerText = "삭제";
     DeleteButton.addEventListener("click", () => {
       memoList.removeChild(div); // memoList.remove(div)로 하면 한 번 삭제 버튼 누른 뒤에 메모추가가 안됨
     });
     DeleteButton.classList.add("delete-button");
+    
+    const CorrectButton = document.createElement("button");
+    CorrectButton.innerText = "수정";
+    CorrectButton.addEventListener("click", () => {
+      if(CorrectButton.disabled){
+        Input.disabled = false;
+        CorrectButton.innerText = "완료";
+      }else{
+        Input.disabled = true;
+        CorrectButton.innerText = "수정";                                                            
+      }
+      
+    })
 
-    div.append(Input, DeleteButton);
-    memoList.appendChild(div); // memoList.append(div); 도 가능
+    div.append(Input, DeleteButton, CorrectButton);
+    memoList.appendChild(div); // memoList.append(div);도 가능
+
   }
 }
 
 const checkInput = () => {
   const input = document.getElementById("memo-text");
   if (input.value !== "") {
+    //로컬스토리지에 저장
+    localStorage.setItem("memoData", input.value);
     new Item(input.value);
   }
   input.value = "";
