0aba002ba146bbebe31dbee8f1760d6028fade1c
Kyla Kim <109022128+bomvomsss@users.noreply.github.com>
2024-03-04 18:51:13 +0900

    코딩애플 시작

---
+++ coding_apple/blog/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+
+  let post = '강남 우동 맛집';
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h3>블로그뀨</h3>
+      </div>
+      <h4>{post}</h4>
+    </div>
+  );
+}
+
+export default App;
