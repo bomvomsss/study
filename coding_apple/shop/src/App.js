e19df4e181300bb5610f8b8c5e7118d21fccc883
Kyla Kim <109022128+bomvomsss@users.noreply.github.com>
2024-05-15 17:11:05 +0900

    코딩애플_라우터

--- coding_apple/shop/src/App.js
+++ coding_apple/shop/src/App.js
@@ -1,38 +1,66 @@
 import { useState } from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import data from './data.js';
 import Item from './Item.js';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from './pages/Detail.js';
 
 function App() {
   const [shoes] = useState(data);
+  const navigate = useNavigate(); //페이지 이동 기능 
 
   return (
-    <div className="App">
+    <div className="App"> 
+
        <Navbar bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">BOMVOMSSS</Navbar.Brand>
+          <Navbar.Brand onClick={()=>{ navigate('/') }}>ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">메인</Nav.Link>
-            <Nav.Link href="#best">베스트</Nav.Link>
-            <Nav.Link href="#new">신상품</Nav.Link>
-            <Nav.Link href="#all">전체상품</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/') }}>Home</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/detail') }}>Detail</Nav.Link>
+            <Nav.Link onClick={()=>{ navigate('/event') }}>Event</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className="main-bg"></div>
-      <Container>
-        <div className='row'>
-          {shoes.map((shoes,index) => {
-            return (
-              <Item shoes={shoes} id={index}></Item>
-            );
-          })} 
+
+      <Routes>
+        <Route path='/' element={
+        <div>
+          <div className="main-bg"></div>
+          <Container>
+            <div className='row'>
+              {shoes.map((shoes,index) => {
+                return (
+                  <Item shoes={shoes} id={index}></Item>
+                );
+              })} 
+            </div>
+          </Container>
         </div>
-      </Container>
+        }/>
+
+        <Route path='/detail' element={<Detail/>}/>
+
+        <Route path='/event' element={<Event/>}>
+          <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>}/>
+          <Route path='two' element={<div>생일기념 쿠폰받기</div>}/>
+        </Route>
+        
+      </Routes>
+
     </div>
   );
+}
+
+function Event(){
+  return(
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  )
 }
 
 export default App;
