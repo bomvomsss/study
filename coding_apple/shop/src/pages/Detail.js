d7e4ba17e49c215cc8fd8c1719f32b9ba0916c14
Kyla Kim <109022128+bomvomsss@users.noreply.github.com>
2024-06-02 21:24:23 +0900

    코딩애플

--- coding_apple/shop/src/pages/Detail.js
+++ coding_apple/shop/src/pages/Detail.js
@@ -1,11 +1,37 @@
 import {useParams} from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function Detail(props){
 
   const {id} = useParams();
+  let [count, setCount] = useState(0);
+  let [box, setBox] = useState(true);
+  let [num, setNum] = useState('');
 
+  useEffect(()=>{
+    let a = setTimeout(() => { setBox(false) }, 2000) // 타이머
+    return ()=>{
+      clearTimeout(a)
+    }
+  }, [])
+  
+ useEffect(()=>{
+  if(isNaN(num) === true){
+    alert('그러지마세요')
+  }
+  }, [num])
+  
   return(
   <div className="container">
+    {
+      box === true
+      ? <div className='alert alert-warning'>
+          2초 이내 구매시 할인
+        </div>
+        : null 
+    }
+    {count} 
+    <button onClick={()=>{setCount(count+1)}}>up</button>
     <div className="row">
       <div className="col-md-6">
         <img src={props.shoes[id].src} width="100%" alt=""/>
@@ -14,6 +40,9 @@ function Detail(props){
         <h4 className="pt-5">{props.shoes[id].title}</h4>
         <p>{props.shoes[id].content}</p>
         <p>{props.shoes[id].price}</p>
+
+        <p><input id="test-target" onChange={(e)=>{setNum(e.target.value)}}/></p>
+        
         <button className="btn btn-danger">주문하기</button> 
       </div>
     </div>
